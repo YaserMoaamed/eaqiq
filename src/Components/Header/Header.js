1736496e44b2c04a15da1ea177116690d14fcded
YaserMoaamed <74364668+YaserMoaamed@users.noreply.github.com>
2020-12-23 21:30:53 +0100

    added react start template
    first setup for the eaqiq project by adding reactjs start template

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav, NavbarContainer } from './Header.element'
+
+const Header = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                </NavbarContainer>    
+            </Nav>
+        </>
+    )
+}
+
+export default Header
